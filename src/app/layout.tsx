8b659bb29a661bf170e91666ee1d0dcf4f1c21cb
Sumit Dhiman <ersumitdhiman@gmail.com>
2025-12-17 13:47:10 +0530

    changing name of app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "your video world",
+  title: "weTube",
   description: "Upload your video & show to the World",
 };
 
